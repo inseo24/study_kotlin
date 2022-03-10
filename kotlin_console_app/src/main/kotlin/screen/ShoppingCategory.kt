aa61129a56956675d359501413df1efdc253fb71
choi seoin <inseo@uupfi.com>
2022-03-11 01:53:07 +0900

    refactor: home화면과 category 화면으로 분리

--- kotlin_console_app/src/main/kotlin/screen/ShoppingCategory.kt
+++ kotlin_console_app/src/main/kotlin/screen/ShoppingCategory.kt
@@ -1,4 +1,31 @@
 package screen
 
 class ShoppingCategory {
+
+    fun showCategories() {
+        val categories = arrayOf("패션", "전자기기", "반려동물용품")
+        for (category in categories) {
+            println(category)
+        }
+
+        println("=> 장바구니로 이동하려면 #을 입력해주세요.")
+
+        var userSelectedCategory = readLine()
+        while (userSelectedCategory.isNullOrBlank()) {
+            println("값을 입력해주세요")
+            userSelectedCategory = readLine()
+        }
+
+        if (userSelectedCategory == "#") {
+            // TODO 1. 장바구니 이동
+        } else {
+            if (categories.contains(userSelectedCategory)) {
+                // TODO 2. 카테고리 상품 목록 보여주기
+
+            } else {
+                println("[${userSelectedCategory}] : 존재하지 않는 카테고리입니다. 다시 입력해주세요.")
+                showCategories()
+            }
+        }
+    }
 }
--- kotlin_console_app/src/main/kotlin/screen/ShoppingHome.kt
+++ kotlin_console_app/src/main/kotlin/screen/ShoppingHome.kt
@@ -21,24 +21,8 @@ class ShoppingHome {
     }
 
     private fun showCategories() {
-        val categories = arrayOf("패션", "전자기기", "반려동물용품")
-        for (category in categories) {
-            println(category)
-        }
-
-        println("=> 장바구니로 이동하려면 #을 입력해주세요.")
-
-        var userSelectedCategory = readLine()
-        while (userSelectedCategory.isNullOrBlank()) {
-            println("값을 입력해주세요")
-            userSelectedCategory = readLine()
-        }
-
-        if (userSelectedCategory == "#") {
-            // TODO 1. 장바구니 이동
-        } else {
-            // TODO 2. 카테고리 상품 목록 보여주기
-            // TODO 3. 사용자가 카테고리 목록에 없는 값을 입력한 경우 처리
-        }
+        val shoppingCategory = ShoppingCategory()
+        shoppingCategory.showCategories()
     }
+
 }
